ea2942a20dd0474c20011600af67b04fefc470a0
Grace-Avor <graceavor2021@gmail.com>
2023-05-30 12:00:14 -0700

    new printf

--- _printf.c
+++ _printf.c
@@ -5,9 +5,8 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format
- * Return: Printed chars
+ * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -54,9 +53,9 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - Prints the contents of the buffer if they exist
  * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length
+ * @buff_ind: Index at which to add next char, represents the length.
  */
 
 void print_buffer(char buffer[], int *buff_ind)
